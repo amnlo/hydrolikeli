4aa9edaea4bbc1cc7eb455c3f7ad6fdda20cdf46
Lorenz Ammann <lorenz.ammann@eawag.ch>
2019-10-24 16:36:14 +0200

    Functions to remove taumax for Q

--- plot.markov.r
+++ plot.markov.r
@@ -362,6 +362,34 @@ layout.q <- list(layout = data.frame(var=rep("C1Wv_Qstream", nrow(sudriv$input$i
     print(summary((q.const - q.step)[2000:length(td)]))
     print(summary(q.const - q.chaos))
 }
+remove.constpar <- function(res, param){
+    ## this function takes a result of 'infer.timedeppar' of the timedeppar package, removes the parameter 'param' from the constant parameters that are inferred and returns the new result object.
+    ##if(res$package != "timedeppar 0.5 2019-10-18") warning("not the version of result I expected...")
+    res.new <- res
+    for(p.curr in param){
+        if(!(p.curr %in% names(res$param.ini))){warning(paste0("parameter ",pucrr," not found"));next}
+        res.new$param.ini[[p.curr]] <- NULL
+        res.new$param.range[[p.curr]] <- NULL
+        res.new$sample.param.const <- res.new$sample.param.const[,colnames(res.new$sample.param.const)!=p.curr]
+        res.new$param.maxpost[[p.curr]] <- NULL
+        rmcnst1 <- rownames(res.new$cov.prop.const)==p.curr
+        rmcnst2 <- colnames(res.new$cov.prop.const)==p.curr
+        res.new$cov.prop.const <- res.new$cov.prop.const[!rmcnst1,!rmcnst2]
+    }
+    return(res.new)
+}
+remove.taumax.Q <- function(sudriv){
+    ## removes the correlation in Q of the error model and removes taumax from the fitted parameters. This is manly needed because the time-dependent parameters replace the correlation in the error model.
+    ind <- which(names(sudriv$likelihood$parameters)=="GLOB_Mult_Q_taumax_lik")
+    sudriv$likelihood$tran[ind] <- 0
+    sudriv$likelihood$lb[ind] <- 0
+    sudriv$likelihood$ub[ind] <- 10000
+    sudriv$likelihood$parameters["GLOB_Mult_Q_taumax_lik"] <- 0
+    sudriv$likelihood$par.fit[ind] <- 0
+    tmp <- dimnames(sudriv$parameter.sample)[[2]]=="GLOB_Mult_Q_taumax_lik"
+    sudriv$parameter.sample <- sudriv$parameter.sample[,!tmp,]
+    return(sudriv)
+}
 redef.init.range <- function(sudriv, drop=0.9, jitter=0, init.range.orig=matrix(0,ncol=2)){
     if(jitter != 0 & is.na(init.range.orig[1])) warning("No init.range.orig applied in case of jitter")
     logpost <- quantile(sudriv$posterior.sample[nrow(sudriv$posterior.sample),], drop)
